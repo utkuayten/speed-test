2d6603e37b37a9e1c8ddfd40065c8b7f59fc1c71
utkuayten <utkuayten7878@outlook.com>
2025-10-28 02:47:08 +0300

    Update Download

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+# server.py
+from flask import Flask, Response, request
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "✅ Flask is working!"
+@app.route("/download")
+def download():
+    size = int(request.args.get("size", 1024 * 1000))  # default 1 MB
+    data = os.urandom(size)
+    return Response(data, mimetype="application/octet-stream")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route("/")
+def home():
+    return open("index.html").read()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, threaded=True)
